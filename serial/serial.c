0ba5281c2346eca5efb762d52bb3d608305539bf
Cosmin Tanislav <demonsingur@gmail.com>
2021-07-22 16:44:04 +0300

    firmware-challenge: reformat code

--- serial/serial.c
+++ serial/serial.c
@@ -4,9 +4,9 @@
 void print_sensor_data(sensor_data_t *sensor_data);
 
 int main() {
-    sensor_data_t sensor_data;
-    get_sensor_data(&sensor_data);
-    print_sensor_data(&sensor_data);
+	sensor_data_t sensor_data;
+	get_sensor_data(&sensor_data);
+	print_sensor_data(&sensor_data);
 }
 
 /** the data from the sensor comes in as following:
@@ -20,8 +20,8 @@ void get_sensor_data(sensor_data_t *sensor_data) {
 }
 
 void print_sensor_data(sensor_data_t *sensor_data) {
-    printf("id: %d\n", sensor_data->id);
-    printf("int_data: %d\n", sensor_data->int_data);
-    printf("short_data: %d\n", sensor_data->short_data);
-    printf("char_data: %d\n", sensor_data->char_data);
+	printf("id: %d\n", sensor_data->id);
+	printf("int_data: %d\n", sensor_data->int_data);
+	printf("short_data: %d\n", sensor_data->short_data);
+	printf("char_data: %d\n", sensor_data->char_data);
 }
