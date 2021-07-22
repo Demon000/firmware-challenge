073203d64c2241049e3428ae87150bd8a471a192
Cosmin Tanislav <demonsingur@gmail.com>
2021-07-22 16:44:14 +0300

    firmware-challenge: solve challenge-3

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ challenge-1: challenge-1.c
 
 challenge-2: challenge-2.c
 	gcc -Wall -g -o $@ $^
+
+challenge-3: serial/serial.c serial/uart_driver.c
+	gcc -Wall -g -o $@ $^
--- serial/serial.c
+++ serial/serial.c
@@ -1,5 +1,7 @@
 #include "uart_driver.h"
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 void print_sensor_data(sensor_data_t *sensor_data);
 
@@ -16,7 +18,29 @@ int main() {
  * 4. char_data     [char,  1 bytes]
  */
 void get_sensor_data(sensor_data_t *sensor_data) {
-    /** complete this function */
+	uart_driver_start_communication();
+
+	/*
+	 * We assume that the endianness of the data matches
+	 * the endianness of the current machine for simplicity.
+	 */
+#define COPY_MEMBER(member)                         \
+	do {                                            \
+		int __size = sizeof(sensor_data->member);   \
+		uint8_t __data[__size];                       \
+		for (int i = 0; i < __size; i++) {          \
+			__data[i] = uart_driver_get_byte();       \
+		}                                           \
+		memcpy(&sensor_data->member, __data, __size); \
+	} while (0)
+
+	COPY_MEMBER(id);
+	COPY_MEMBER(int_data);
+	COPY_MEMBER(short_data);
+	COPY_MEMBER(char_data);
+
+#undef COPY_MEMBER
+	uart_driver_stop_communication();
 }
 
 void print_sensor_data(sensor_data_t *sensor_data) {
